5803b67a50238690c7946a3ac7b3674cda4cc960
Jose Jerez <joseignaciojerez@gmail.com>
2020-03-11 03:00:42 -0300

    model

--- app/controllers/trabajos_controller.rb
+++ app/controllers/trabajos_controller.rb
@@ -2,13 +2,13 @@ class TrabajosController < ApplicationController
   before_action :set_trabajo, only: [:show, :edit, :update, :destroy]
 
   # GET /trabajos
-  # GET /trabajos.json
+
   def index
     @trabajos = Trabajo.all
   end
 
   # GET /trabajos/1
-  # GET /trabajos/1.json
+
   def show
   end
 
@@ -22,42 +22,42 @@ class TrabajosController < ApplicationController
   end
 
   # POST /trabajos
-  # POST /trabajos.json
+
   def create
     @trabajo = Trabajo.new(trabajo_params)
 
     respond_to do |format|
       if @trabajo.save
         format.html { redirect_to @trabajo, notice: 'Trabajo was successfully created.' }
-        format.json { render :show, status: :created, location: @trabajo }
+
       else
         format.html { render :new }
-        format.json { render json: @trabajo.errors, status: :unprocessable_entity }
+
       end
     end
   end
 
   # PATCH/PUT /trabajos/1
-  # PATCH/PUT /trabajos/1.json
+
   def update
     respond_to do |format|
       if @trabajo.update(trabajo_params)
         format.html { redirect_to @trabajo, notice: 'Trabajo was successfully updated.' }
-        format.json { render :show, status: :ok, location: @trabajo }
+
       else
         format.html { render :edit }
-        format.json { render json: @trabajo.errors, status: :unprocessable_entity }
+
       end
     end
   end
 
   # DELETE /trabajos/1
-  # DELETE /trabajos/1.json
+
   def destroy
     @trabajo.destroy
     respond_to do |format|
       format.html { redirect_to trabajos_url, notice: 'Trabajo was successfully destroyed.' }
-      format.json { head :no_content }
+
     end
   end
 
--- app/models/trabajo.rb
+++ app/models/trabajo.rb
@@ -1,2 +1,4 @@
 class Trabajo < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
